b10a01e916f8f61715e4234cda2d434699649c30
Maria Beatriz <beatrizflorenccio@gmail.com>
2020-06-06 16:14:17 -0300

    Aula01 e 02

---
+++ aula02/aula02.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //entrada e saída
+
+using namespace std; //namespace standard
+
+int main() { //Pode retornar um valor do tipo inteiro
+    //Bloco de instrução
+    cout << "Hello World!\n"; //Console Ouput
+    return 0;
+}
